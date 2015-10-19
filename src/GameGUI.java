a6fa0b7be63588cbaa18c5a70eaf2ce86ea5e0ce
mldeanha <mldeanha@mtu.edu>
2015-10-19 14:09:09 -0400

    master

---
+++ src/GameGUI.java
@@ -0,0 +1,39 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Class which contains the Graphical Use Interface for
+ * the Team 4 project SudokuPlus
+ * 
+ * 					Does it need to extend JFrame?
+ * 					Do more research
+ * @author Matt
+ *
+ */
+public class GameGUI extends JFrame implements ActionListener{
+	
+	/**
+	 * Constructor class for GameGUI
+	 */
+	public GameGUI(){
+		
+	}
+
+	/**
+	 * ActionListener method for detecting user input
+	 * @param arg0
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Main method for running GUI
+	 */
+	public void main(){
+		
+	}
+}
