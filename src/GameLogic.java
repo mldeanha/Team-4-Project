5608710966ffff9a68ad217e91228eae3614043d
Mitchell Baer <mjbaer@c28-0117-10.ad.mtu.edu>
2015-10-20 13:09:17 -0400

    Fixed .txt files

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 /**
  * This class handles all of the game logic. That
  * will be able to accessed by the server and the GUI.
@@ -16,13 +19,33 @@ public class GameLogic {
 	boolean guessVariable;
 
 	//Contains all of the values of the current puzzle stored in an array.
-	int[][] currentPuzzle;
+	static ArrayList<String> currentPuzzle = new ArrayList<String>();
 
 	//Contains all of the valeus of the current puzzle's solution.
-	int[][] currentPuzzleSolution;
+	static ArrayList<String> currentPuzzleSolution = new ArrayList<String>();
 
 	//Integer (1,2 or 3) that sets the difficulty of the puzzle.
 	int difficulty;
+
+	public static void main(String[] args)
+	{
+
+		GameLogic tester = new GameLogic();
+
+		try {
+			tester.readPuzzle();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		for(int i=0;i<81;i++){
+			System.out.println("PUZZLE ELEMENT " + (i+1) + ":" + currentPuzzle.get(i));
+		}
+
+		for(int i=0;i<81;i++){
+			System.out.println("PUZZLE SOLUTION ELEMENT " + (i+1) + ":" + currentPuzzleSolution.get(i));
+		}
+	}
 
 	/**
 	 * 
@@ -49,4 +72,17 @@ public class GameLogic {
 
 	}
 
+	public void readPuzzle() throws FileNotFoundException{
+
+		Scanner scanner = new Scanner(new File("SudokuPuzzles/easypuzzle1.txt"));
+		Scanner scanner1 = new Scanner(new File("SudokuPuzzles/easypuzzle1solution.txt"));
+
+		for(int i=0;i<81;i++){
+			currentPuzzle.add(i,scanner.nextLine());
+		}
+
+		for(int i=0;i<81;i++){
+			currentPuzzleSolution.add(i,scanner1.nextLine());
+		}
+	}
 }
