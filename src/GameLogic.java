41fd3a2bf489f2bee214c2a9973d2cd3f74f61e1
Mitchell Baer <mjbaer@c28-0117-04.ad.mtu.edu>
2015-10-21 15:08:35 -0400

    Worked on Game Logic. Got random puzzle generation working.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -12,6 +12,8 @@ import java.util.*;
  */
 public class GameLogic {
 
+	//Global Scanner that can be set 
+
 	//CONTAINS 3 VALUES: [Input Value, Coordinate X, Coordinate Y]
 	int[] userInputDataPosition;
 
@@ -19,18 +21,19 @@ public class GameLogic {
 	boolean guessVariable;
 
 	//Contains all of the values of the current puzzle stored in an array.
-	static ArrayList<String> currentPuzzle = new ArrayList<String>();
+	static String[][] currentPuzzle = new String[9][9];
 
-	//Contains all of the valeus of the current puzzle's solution.
-	static ArrayList<String> currentPuzzleSolution = new ArrayList<String>();
+	//Contains all of the values of the current puzzle's solution.
+	static String[][] currentPuzzleSolution = new String[9][9];
 
 	//Integer (1,2 or 3) that sets the difficulty of the puzzle.
-	int difficulty;
+	static int difficulty;
 
 	public static void main(String[] args)
 	{
 
 		GameLogic tester = new GameLogic();
+		difficulty = 1;
 
 		try {
 			tester.readPuzzle();
@@ -38,12 +41,20 @@ public class GameLogic {
 			e.printStackTrace();
 		}
 
-		for(int i=0;i<81;i++){
-			System.out.println("PUZZLE ELEMENT " + (i+1) + ":" + currentPuzzle.get(i));
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				System.out.print(currentPuzzle[i][j]);
+			}
+			System.out.println();
 		}
 
-		for(int i=0;i<81;i++){
-			System.out.println("PUZZLE SOLUTION ELEMENT " + (i+1) + ":" + currentPuzzleSolution.get(i));
+		System.out.println();
+
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				System.out.print(currentPuzzleSolution[i][j]);
+			}
+			System.out.println();
 		}
 	}
 
@@ -74,15 +85,66 @@ public class GameLogic {
 
 	public void readPuzzle() throws FileNotFoundException{
 
-		Scanner scanner = new Scanner(new File("SudokuPuzzles/easypuzzle1.txt"));
-		Scanner scanner1 = new Scanner(new File("SudokuPuzzles/easypuzzle1solution.txt"));
+		Scanner scanner = null;
+		Scanner scanner1 = null;
 
-		for(int i=0;i<81;i++){
-			currentPuzzle.add(i,scanner.nextLine());
+		switch(difficulty){
+
+		case 1:
+			//IF DIFFICULTY = 1, EASY PUZZLE
+			scanner = new Scanner(new File("SudokuPuzzles/easypuzzles.txt"));
+			scanner1 = new Scanner(new File("SudokuPuzzles/easypuzzlessolutions.txt"));
+			break;
+
+
+		case 2:
+			//IF DIFFICULTY = 2, MEDIUM PUZZLE (STANDARD)
+			scanner = new Scanner(new File("SudokuPuzzles/mediumpuzzles.txt"));
+			scanner1 = new Scanner(new File("SudokuPuzzles/mediumpuzzlessolutions.txt"));
+			break;
+
+		case 3:
+			//IF DIFFICULTY = 3, HARD PUZZLE
+			scanner = new Scanner(new File("SudokuPuzzles/hardpuzzles.txt"));
+			scanner1 = new Scanner(new File("SudokuPuzzles/hardpuzzlessolutions.txt"));
+			break;
+
 		}
 
-		for(int i=0;i<81;i++){
-			currentPuzzleSolution.add(i,scanner1.nextLine());
+		//GENERATES A RANDOM NUMBER THAT WILL SELECT THE PUZZLE OF THE GIVEN DIFFICULTY!
+		Random rand = new Random();
+		int randomSelector = rand.nextInt((2-0)+1)+0;
+		int puzzleSelectorNumber = ((randomSelector)*9)+1;
+
+		int j = 0;
+
+		for(int i=1;i<=((randomSelector+1)*9);i++){
+
+			if(i<puzzleSelectorNumber){
+				scanner.nextLine();
+				continue;
+			}
+
+			String line = scanner.nextLine();
+			String[] split = line.split(",");
+			currentPuzzle[j] = split;
+			j++;
 		}
+
+		int k = 0;
+
+		for(int i=1;i<=((randomSelector+1)*9);i++){
+
+			if(i<puzzleSelectorNumber){
+				scanner1.nextLine();
+				continue;
+			}
+
+			String line = scanner1.nextLine();
+			String[] split = line.split(",");
+			currentPuzzleSolution[k] = split;
+			k++;
+		}
+
 	}
 }
